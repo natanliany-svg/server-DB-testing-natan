7fca96816eeb16a38f09a2c925d6ebd88a10d6b1
Natan Liany <natanliany@gmail.com>
2026-07-29 11:56:31 +0300

    השלמת מבנה הקבצים וכן הרצת שרת בסיסי

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+//בסיעתא דשמייא 
+import express from 'express'
+
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+//app.use('/',router)
+
+app.listen(PORT ,(e) => {
+    if (e) return console.error('server filed',e.message);
+    console.log('server is running in port' , PORT);
+})
 
 
 
